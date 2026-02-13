3da08f453c070da4d316d4036f5ef6c963746afe
ztrawhcs <matthew.s.schwartz@gmail.com>
2026-02-13 13:28:26 -0500

    Update EpdFontFamily.h

--- lib/EpdFont/EpdFontFamily.h
+++ lib/EpdFont/EpdFontFamily.h
@@ -3,11 +3,12 @@
 
 class EpdFontFamily {
  public:
-enum Style : uint8_t { REGULAR = 0, BOLD = 1, ITALIC = 2, BOLD_ITALIC = 3, UNDERLINE = 4 };
+  enum Style : uint8_t { REGULAR = 0, BOLD = 1, ITALIC = 2, BOLD_ITALIC = 3, UNDERLINE = 4 };
 
   static bool globalForceBold;
 
   explicit EpdFontFamily(const EpdFont* regular, const EpdFont* bold = nullptr, const EpdFont* italic = nullptr,
+                         const EpdFont* boldItalic = nullptr)
       : regular(regular), bold(bold), italic(italic), boldItalic(boldItalic) {}
   ~EpdFontFamily() = default;
   void getTextDimensions(const char* string, int* w, int* h, Style style = REGULAR) const;
